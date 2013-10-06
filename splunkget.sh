cf5f403f9223c76a469e213fdab44cf3d78c0556
Michael Chamberland <smokex365@gmail.com>
2013-10-06 14:27:44 -0400

    Changed Dropbox Links to GitHub Raw File links for future development

--- splunkget.sh
+++ splunkget.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
  
 # Created by Michael Chamberland
-# Site: http://aramblinggeek.wordpress.com
-# Version: 1.13
-# Last Updated: 4/20/2013 6:45PM
-# What it does: This script checks for a new version of the upgrade
-# script of my Dropbox account.
- 
-# 1. If the upgrade script is not found in the local folder it will
-#        download it from the web.
-# 2. If the upgrade script is found then it checks the MD5 checksums
-#       against the version downloaded from the web. If they are different
-#       it keeps the web version, otherwise it will keep the current copy.
-#
-# Once all checks have finished it will prompt the user to update and if
-#       user confirms it will run the upgrade.
- 
-# Checks for Splunk upgrade script. If not found, download from dropbox.
-# If found, proceed to MD5 checks.
+# Site: http://www.aramblinggeek.com
+
 if [ ! -f 'splunku.sh' ]
     then
-    wget -q --secure-protocol='auto' -O 'splunku.sh' 'https://www.dropbox.com/sh/yjw4uewbrmotrpc/l0bAMs3DiO/splunku.sh?dl=1'
+    wget -q --secure-protocol='auto' -O 'splunku.sh' 'https://raw.github.com/Smokex365/Splunk_Upgrade_Scripts/master/splunku.sh'
     chmod u+x splunku.sh
 else
-    wget -q --secure-protocol='auto' -O 'splunku.sh.tmp' 'https://www.dropbox.com/sh/yjw4uewbrmotrpc/l0bAMs3DiO/splunku.sh?dl=1'
+    wget -q --secure-protocol='auto' -O 'splunku.sh.tmp' 'https://raw.github.com/Smokex365/Splunk_Upgrade_Scripts/master/splunku.sh'
     chmod u+x splunku.sh.tmp
     md5t=`md5sum splunku.sh.tmp | cut -c 1-32`
     md5o=`md5sum splunku.sh | cut -c 1-32`
